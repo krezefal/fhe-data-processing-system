6d8b39047e8b453902dffb50cca59843983caf23
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-06-06 03:59:09 +0300

    fix multiple queries' bugs; add less broad error handling; reorg class behavior; add colorful out

--- db_api.py
+++ db_api.py
@@ -1,6 +1,6 @@
 import pickle
 from typing import Tuple
-
+from colorama import Fore, Style
 import psycopg2
 from psycopg2 import sql
 import numpy as np
@@ -14,150 +14,155 @@ class DBConn:
         self.user = user
         self.password = password
 
+        self.cur = None
         try:
             self.conn = psycopg2.connect(f"dbname={self.db_name} user={self.user} password={self.password}")
-            self.cur = self.conn.cursor()
         except ConnectionError:
-            print("[DBConn]: Some error occurred during connection establishment")
+            print(f"{Fore.RED}Error while connection establishment{Style.RESET_ALL}")
 
     def create_user(self, data: tuple):
         try:
-            # data = tuple(data.values())
+            self.cur = self.conn.cursor()
             user_tokens = sql.SQL(', ').join(sql.Literal(n) for n in data)
 
-            query = sql.SQL("INSERT INTO person (login, password, public_key) VALUES ({values})").format(
+            query = sql.SQL("INSERT INTO person (login, passwd_hash, public_key) VALUES ({values})").format(
                 values=user_tokens
             )
 
             self.cur.execute(query)
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while inserting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
     def get_user_info(self, login: str):
         try:
+            self.cur = self.conn.cursor()
             query = sql.SQL("SELECT * FROM person WHERE login = {login}").format(
-                login=sql.Identifier(login),
+                login=sql.Literal(login)
             )
 
             self.cur.execute(query)
             user_info = self.cur.fetchall()
             return user_info
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while getting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
-    def get_variable(self, login: str, name: bytes):
+    def get_variable(self, login: str, name: str):
         try:
-            query = sql.SQL("SELECT * FROM memory WHERE login = {login} and name = {name}").format(
-                login=sql.Identifier(login),
-                name=sql.Identifier(name),
+            self.cur = self.conn.cursor()
+            query = sql.SQL("SELECT * FROM memory WHERE login = {login} and var_name = {name}").format(
+                login=sql.Literal(login),
+                name=sql.Literal(name)
             )
 
             self.cur.execute(query)
             variable_fields = self.cur.fetchall()
             return variable_fields
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while getting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
     def get_memory(self, login: str):
         try:
+            self.cur = self.conn.cursor()
             query = sql.SQL("SELECT * FROM memory WHERE login = {login}").format(
-                login=sql.Identifier(login),
+                login=sql.Literal(login),
             )
 
             self.cur.execute(query)
             dump = self.cur.fetchall()
             return dump
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while getting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
-    def check_variable_exist(self, login: str, name: bytes) -> bool:
+    def check_variable_exist(self, login: str, name: str) -> bool:
         try:
-            query = sql.SQL("SELECT 1 FROM memory WHERE login = {login} and name = {name}").format(
-                login=sql.Identifier(login),
-                name=sql.Identifier(name),
+            self.cur = self.conn.cursor()
+            query = sql.SQL("SELECT 1 FROM memory WHERE login = {login} and var_name = {name}").format(
+                login=sql.Literal(login),
+                name=sql.Literal(name),
             )
 
             self.cur.execute(query)
             response = self.cur.fetchall()
 
-            if response is None:
-                return False
-            return True
+            return response
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while getting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
     def init_new_variable(self, var_data: tuple):
         try:
-            # data = tuple(data.values())
+            self.cur = self.conn.cursor()
             var_tokens = sql.SQL(', ').join(sql.Literal(n) for n in var_data)
 
-            query = sql.SQL("INSERT INTO memory (var_name, login, value) VALUES ({var_tokens})").format(
+            query = sql.SQL("INSERT INTO memory (var_name, login, value) VALUES ({values})").format(
                 values=var_tokens
             )
 
             self.cur.execute(query)
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while inserting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
-    def edit_value(self, login: str, name: bytes, value: bytes):
+    def edit_value(self, login: str, name: str, value: bytes):
         try:
-
-            query = sql.SQL("UPDATE memory SET value = {value} WHERE login = {login} and name = {name}").format(
-                login=sql.Identifier(login),
-                name=sql.Identifier(name),
-                value=sql.Identifier(value)
+            self.cur = self.conn.cursor()
+            query = sql.SQL("UPDATE memory SET value = {value} WHERE login = {login} and var_name = {name}").format(
+                login=sql.Literal(login),
+                name=sql.Literal(name),
+                value=sql.Literal(value)
             )
 
             self.cur.execute(query)
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while updating data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
-    def delete_variable(self, login: str, name: bytes):
+    def delete_variable(self, login: str, name: str):
         try:
-
-            query = sql.SQL("DELETE FROM memory WHERE login = {login} and name = {name}").format(
-                login=sql.Identifier(login),
-                name=sql.Identifier(name),
+            self.cur = self.conn.cursor()
+            query = sql.SQL("DELETE FROM memory WHERE login = {login} and var_name = {name}").format(
+                login=sql.Literal(login),
+                name=sql.Literal(name),
             )
 
             self.cur.execute(query)
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while deleting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
 
-    def calc_variables(self, login: str, name1: bytes, name2: bytes, op: str) -> Tuple[np.poly1d, np.poly1d, np.poly1d]:
+    def calc_variables(self, login: str, name1: bytes, name2: bytes, op: str) \
+            -> Tuple[np.poly1d, np.poly1d, np.poly1d]:
         try:
+            self.cur = self.conn.cursor()
+
             variable1 = self.get_variable(login, name1)
             variable2 = self.get_variable(login, name2)
 
@@ -172,13 +177,13 @@ class DBConn:
                 return enc_value1 * enc_value2, None, None
             if op == "/":
                 if enc_value2 == 0:
-                    raise Exception('Zero division')
+                    raise Exception(f"{Fore.RED}Zero division{Style.RESET_ALL}")
 
                 enc_q, enc_r = enc_value1 / enc_value2
                 return enc_q, enc_r, enc_value2
 
-        except Exception:
-            print("[DBConn]: An error occurred during query execution")
+        except (Exception, psycopg2.Error) as error:
+            print(f"{Fore.RED}Error while getting data: {error}{Style.RESET_ALL}")
         finally:
             self.cur.close()
             self.conn.commit()
