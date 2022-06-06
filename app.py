c96e78d4a8b260f8e4a008ecaf592d74d4236864
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-06-06 03:54:55 +0300

    add exception handling and colorful out; change methods behaviour => reorganize the code

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
-from interface import Interface
+from colorama import Fore, Style
 
-_interface = Interface()
+from interface import Interface
 
 
 def begin():
+    _interface = Interface()
+
     while True:
 
         print("'0' -> Exit")
         print("'1' -> Sign in")
         print("'2' -> Sign up")
 
-        choice = int(input("Enter task number: "))
+        while True:
+            try:
+                choice = int(input("Enter task number: "))
+                break
+            except ValueError:
+                print(f"{Fore.RED}Number required{Style.RESET_ALL}")
 
         match choice:
             case 0:
+                del _interface
                 return
             case 1:
-                auth = _interface.sign_in()
-                if auth:
-                    _interface.operate()
-                else:
-                    print("Invalid login or password")
+                _interface.sign_in()
+                _interface.operate()
             case 2:
                 _interface.sign_up()
             case _:
-                print("Invalid action")
+                print(f"{Fore.RED}Invalid action{Style.RESET_ALL}")
 
 
 if __name__ == "__main__":
