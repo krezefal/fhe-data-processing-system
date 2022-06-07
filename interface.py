e786661254ecf93e41e6b3cbd79eb55172019e90
Timofey Klimov <klimovtimofeyy@gmail.com>
2022-06-07 21:23:48 +0300

    remove public key sending to db; improve enc vars division; improve whole calc operation case to write the result into initialized var; reorganize and clean the methods; add name quiz while signing up

--- interface.py
+++ interface.py
@@ -1,4 +1,3 @@
-from getpass import getpass
 from hashlib import md5
 from colorama import Fore, Style
 import pickle
@@ -7,7 +6,7 @@ import yaml
 from abramov_system import keygen
 from db_api import DBConn
 from consts import *
-import utils
+import interface_utils
 
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
@@ -27,41 +26,22 @@ class Interface:
         db.close_connection()
 
     def sign_up(self):
-        while True:
-            login = input("Login: ")
-            response = db.get_user_info(login)
-            if response:
-                print(f"{Fore.RED}User with this login already exists. Try another{Style.RESET_ALL}")
-            else:
-                break
+        print(f"{Fore.CYAN}Signing up: {Fore.WHITE}name{Style.RESET_ALL} -> login -> password")
+        login = interface_utils.signup_login(db)
 
-        while True:
-            password = getpass("Password: ")
-            confirm = getpass("Confirm Password: ")
-            if password != confirm:
-                print(f"{Fore.RED}Passwords are not the same{Style.RESET_ALL}")
-            else:
-                break
+        print(f"{Fore.CYAN}Signing up: {Style.RESET_ALL} name -> {Fore.WHITE}login{Style.RESET_ALL} -> password")
+        name = input("Enter name: ")
+
+        print(f"{Fore.CYAN}Signing up: {Style.RESET_ALL} name -> login -> {Fore.WHITE}password{Style.RESET_ALL}")
+        password = interface_utils.signup_password()
 
         print("To use remote secure memory you need private and public keys.")
         print("Both will be generated automatically after you enter the parameters:")
 
-        while True:
-            try:
-                base = int(input("Number system: "))
-                degree = int(input("Degree of the key polynomial: "))
-                break
-            except ValueError:
-                print(f"{Fore.RED}That was no valid number{Style.RESET_ALL}")
+        base, degree = interface_utils.enter_alg_params()
+        self.private_key, self.public_key = keygen.generate_abramov_keypair(base, degree)
 
-        self.private_key, self.public_key \
-            = keygen.generate_abramov_keypair(base, degree)
-
-        # db.create_user({"login": login,
-        #                 "passwd_hash": md5(password.encode('utf-8')).hexdigest(),
-        #                 "public_key": pickle.dumps(self.public_key)})
-
-        db.create_user((login, md5(password.encode('utf-8')).hexdigest(), pickle.dumps(self.public_key)))
+        db.create_user((login, name, md5(password.encode('utf-8')).hexdigest()))
 
         with open('keys/private_key.fhe', 'wb') as file_private_key, \
                 open('keys/public_key.fhe', 'wb') as file_public_key:
@@ -72,40 +52,18 @@ class Interface:
         print(f"{Fore.GREEN}Private and public keys were saved to 'keys' directory{Style.RESET_ALL}")
 
     def sign_in(self):
-        while True:
-            login = input("Login: ")
-            response = db.get_user_info(login)
-            if not response:
-                print(f"{Fore.RED}User not found{Style.RESET_ALL}")
-            else:
-                break
+        login, resp = interface_utils.response_login(db)
 
-        correct_password = response[0][PASSWORD_HASH]
-        while True:
-            password = getpass("Password: ")
-            if correct_password != md5(password.encode("utf-8")).hexdigest():
-                print(f"{Fore.RED}Wrong password{Style.RESET_ALL}")
-            else:
-                break
+        correct_password = resp[0][PASSWORD_HASH]
+        interface_utils.signin_password(correct_password)
 
         self.login = login
         print(f"{Fore.GREEN}You have successfully authorize{Style.RESET_ALL}")
 
-        while True:
-            try:
-                file_path = str(input("Specify the path to private key file: "))
-                with open(file_path, 'rb') as file_private_key:
-                    self.private_key = pickle.loads(file_private_key.read())
-                break
-            except OSError as error:
-                print(f"{Fore.RED}{error}{Style.RESET_ALL}")
+        self.private_key = interface_utils.enter_key(key_type='private')
+        self.public_key = interface_utils.enter_key(key_type='public')
 
-        self.public_key = pickle.loads(response[0][PUBLIC_KEY])
-
-        # print(self.private_key.get_root())          debug
-        # print(self.public_key.get_polynomial())
-
-        print(f"{Fore.GREEN}Private key has been added{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}Keys has been added{Style.RESET_ALL}")
 
     def operate(self):
         while True:
@@ -121,13 +79,7 @@ class Interface:
             print("             '12' -> Update private and public keys")
             print()
 
-            while True:
-                try:
-                    choice = int(input("Enter operation number: "))
-                    break
-                except ValueError:
-                    print(f"{Fore.RED}Number required{Style.RESET_ALL}")
-
+            choice = interface_utils.enter_option("Enter operation number: ")
             match choice:
 
                 case 0:
@@ -137,13 +89,13 @@ class Interface:
                     return
 
                 case 1:  # Initialize new variable
-                    name = utils.enter_name(self.public_key)
+                    name = str(input("Enter a variable name: "))
                     exist = db.check_variable_exist(self.login, name)
                     if exist:
                         print(f"{Fore.RED}Error: value '{name}' already initialized{Style.RESET_ALL}")
                         continue
 
-                    enc_value, value = utils.enter_value(self.public_key)
+                    enc_value, value = interface_utils.enter_value(self.public_key)
                     db.init_new_variable((name, self.login, pickle.dumps(enc_value)))
                     print(f"{Fore.GREEN}Initialized: {name} = {value}{Style.RESET_ALL}")
 
@@ -159,7 +111,7 @@ class Interface:
                         print(f"{Fore.GREEN}No initialized variables{Style.RESET_ALL}")
 
                 case 3:  # Get variable
-                    name = utils.enter_name(self.public_key)
+                    name = str(input("Enter a variable name: "))
                     variable = db.get_variable(self.login, name)
                     if variable:
                         value = self.private_key.decrypt(pickle.loads(variable[0][VALUE]))
@@ -168,18 +120,18 @@ class Interface:
                         print(f"{Fore.RED}No variable with name {name}{Style.RESET_ALL}")
 
                 case 4:  # Edit value
-                    name = utils.enter_name(self.public_key)
+                    name = str(input("Enter a variable name: "))
                     exist = db.check_variable_exist(self.login, name)
                     if not exist:
                         print(f"{Fore.RED}No variable with name {name}{Style.RESET_ALL}")
                         continue
 
-                    enc_value, value = utils.enter_value(self.public_key)
+                    enc_value, value = interface_utils.enter_value(self.public_key)
                     db.edit_value(self.login, name, pickle.dumps(enc_value))
                     print(f"{Fore.GREEN}Edited: {name} = {value}{Style.RESET_ALL}")
 
                 case 5:  # Delete variable
-                    name = utils.enter_name(self.public_key)
+                    name = str(input("Enter a variable name: "))
                     exist = db.check_variable_exist(self.login, name)
                     if not exist:
                         print(f"{Fore.RED}No variable with name {name}{Style.RESET_ALL}")
@@ -189,8 +141,9 @@ class Interface:
                     print(f"{Fore.GREEN}Variable '{name}' was deleted{Style.RESET_ALL}")
 
                 case 6 | 7 | 8 | 9:  # a +/-/*/'/' b
-                    name1 = utils.enter_name(self.public_key)
-                    name2 = utils.enter_name(self.public_key)
+                    name1 = str(input("Enter 1st variable name: "))
+                    name2 = str(input("Enter 2nd variable name: "))
+                    res_name = str(input("Enter variable name in which to write the result: "))
 
                     exist = db.check_variable_exist(self.login, name1)
                     if exist is False:
@@ -200,30 +153,40 @@ class Interface:
                     if exist is False:
                         print(f"{Fore.RED}No variable with name {name2}{Style.RESET_ALL}")
                         continue
+                    exist = db.check_variable_exist(self.login, res_name)
+                    if exist is False:
+                        print(f"{Fore.RED}No variable with name {res_name}{Style.RESET_ALL}")
+                        continue
 
                     if choice == 6:
                         enc_result, _, _ = db.calc_variables(self.login, name1, name2, op='+')
+                        db.edit_value(self.login, res_name, pickle.dumps(enc_result))
                         result = self.private_key.decrypt(enc_result)
                         print(f"{Fore.GREEN}{name1} + {name2} = {result}{Style.RESET_ALL}")
 
                     if choice == 7:
                         enc_result, _, _ = db.calc_variables(self.login, name1, name2, op='-')
+                        db.edit_value(self.login, res_name, pickle.dumps(enc_result))
                         result = self.private_key.decrypt(enc_result)
                         print(f"{Fore.GREEN}{name1} - {name2} = {result}{Style.RESET_ALL}")
 
                     if choice == 8:
                         enc_result, _, _ = db.calc_variables(self.login, name1, name2, op='*')
+                        db.edit_value(self.login, res_name, pickle.dumps(enc_result))
                         result = self.private_key.decrypt(enc_result)
                         print(f"{Fore.GREEN}{name1} * {name2} = {result}{Style.RESET_ALL}")
 
                     if choice == 9:
                         try:
                             enc_q, enc_r, enc_value2 = db.calc_variables(self.login, name1, name2, op='/')
-                            dec_q = self.private_key.decrypt(enc_q)
-                            dec_r = self.private_key.decrypt(enc_r)
-                            dec_value2 = self.private_key.decrypt(enc_value2)
+                            if enc_r is None and enc_value2 is None:
+                                result = self.private_key.decrypt(enc_q)
+                            else:
+                                dec_q = self.private_key.decrypt(enc_q)
+                                dec_r = self.private_key.decrypt(enc_r)
+                                dec_value2 = self.private_key.decrypt(enc_value2)
+                                result = dec_q + dec_r / dec_value2
 
-                            result = dec_q + dec_r / dec_value2
                             print(f"{Fore.GREEN}{name1} / {name2} = {result}{Style.RESET_ALL}")
 
                         except ZeroDivisionError:
@@ -263,4 +226,5 @@ class Interface:
                           f"and saved to 'keys' directory{Style.RESET_ALL}")
 
                 case _:
-                    print(f"{Fore.RED}Undefined option{Style.RESET_ALL}")
+                    interface_utils.print_error(f"{Fore.RED}Undefined option{Style.RESET_ALL}")
+
